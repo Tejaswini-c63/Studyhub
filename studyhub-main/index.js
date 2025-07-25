537b21d72b3976d5ae836de516fcba24ef25ef73
Tejaswini <tejaswini2004chandrashekar@gmail.com>
2025-07-25 15:21:53 +0530

    first commit

---
+++ studyhub-main/index.js
@@ -0,0 +1,142 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBgHyD8mbJVDlyUq69Dg_wiNC0XeD3qAK8",
+  authDomain: "login-with-firebase-data-3abab.firebaseapp.com",
+  projectId: "login-with-firebase-data-3abab",
+  storageBucket: "login-with-firebase-data-3abab.appspot.com",
+  messagingSenderId: "668790169301",
+  appId: "1:668790169301:web:7de7034794ebe2dd4f8c5c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize variables
+const auth = firebase.auth();
+const database = firebase.database();
+
+// Set up our register function
+function register() {
+  // Get all our input fields
+  email = document.getElementById('email').value;
+  password = document.getElementById('password').value;
+  full_name = document.getElementById('full_name').value;
+  favourite_song = document.getElementById('favourite_song').value;
+  milk_before_cereal = document.getElementById('milk_before_cereal').value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Outta Line!!');
+    return;
+  }
+  if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false) {
+    alert('One or More Extra Fields is Outta Line!!');
+    return;
+  }
+
+  // Move on with Auth
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        email: email,
+        full_name: full_name,
+        favourite_song: favourite_song,
+        milk_before_cereal: milk_before_cereal,
+        last_login: Date.now()
+      };
+
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data);
+
+      // Done
+      alert('User Created!!');
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Set up our login function
+function login() {
+  // Get all our input fields
+  email = document.getElementById('email').value;
+  password = document.getElementById('password').value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Outta Line!!');
+    return;
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        last_login: Date.now()
+      };
+
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data);
+
+      // Done
+      alert('User Logged In!!');
+      // Redirect to index.html
+      window.location.href = 'index2.html';
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  if (expression.test(email) == true) {
+    // Email is good
+    return true;
+  } else {
+    // Email is not good
+    return false;
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password.length < 6) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false;
+  }
+
+  if (field.length <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
